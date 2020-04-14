318e563ac5795d85633ff089fb19765ebf17ed19
Suzy <suzy.nakayama@gmail.com>
2020-04-13 21:05:10 -0400

    Updates

---
+++ precache-manifest.ed4e393f674b9a196c0f4a78fa38c5ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e19606e91274b194f5098bc8e70ae4a",
+    "url": "/typing-race-game/index.html"
+  },
+  {
+    "revision": "93dfaa20432f67e41293",
+    "url": "/typing-race-game/static/css/main.82de018a.chunk.css"
+  },
+  {
+    "revision": "71b89affdb96eaaaf015",
+    "url": "/typing-race-game/static/js/2.fd3ed373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-race-game/static/js/2.fd3ed373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dfaa20432f67e41293",
+    "url": "/typing-race-game/static/js/main.8d3f50cd.chunk.js"
+  },
+  {
+    "revision": "34d4dd17d20533511d03",
+    "url": "/typing-race-game/static/js/runtime-main.a2a6d0c8.js"
+  }
+]);
